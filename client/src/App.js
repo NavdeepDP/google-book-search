87bf3a09b672407025a02d7ddac19b33daf7eda3
NP <navdeep.puri@gmail.com>
2020-11-07 08:39:22 -0500

    add save and searcg routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import Search from "./pages/Search/Search";
+import Save from "./pages/Save/Save";
 
 function App() {
   return (
     <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+      <div className="App-header">        
+        <h2>Welcome to Google Books Search(React)</h2>
+        <h4>Search and Save books of your Interest</h4>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
+
+      <Router>
+      <div >
+        
+        <Route exact path="/" component={Search} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/save" component={Save} />
+
+      </div>
+    </Router>
+
     </div>
   );
 }
